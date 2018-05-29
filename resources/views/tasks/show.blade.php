7ceb9a8ed2c8fd79e7e2e1d26d8aa761c65f779d
sumika.ishikawa <sumika.ishikawa@rakuten.com>
2018-05-29 07:15:04 +0000

    Procfile

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -19,11 +19,20 @@
         </tr>
     </table>
     
+    　　<div class="col-xs-12">
+        <div class="col-sm-offset-2 col-sm-8">
+        <div class="col-md-offset-2 col-md-8">
+        <div class="col-lg-offset-3 col-lg-6">
+    
      {!! link_to_route('tasks.edit', 'このメッセージを編集', ['id' => $task->id], ['class' => 'btn btn-default']) !!}
     
      {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
         {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
     {!! Form::close() !!}
+        </div>
+        </div>
+        </div>
+        </div>
 
 @endsection
